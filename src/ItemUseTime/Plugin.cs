72c7fca3c57ac6006e4cefcd00bb6a8a718959f8
jkqt <jkqtkr@gmail.com>
2025-07-02 10:35:01 -0400

    added conditional for rope spool

--- src/ItemUseTime/Plugin.cs
+++ src/ItemUseTime/Plugin.cs
@@ -12,8 +12,8 @@ namespace ItemUseTime;
 public partial class Plugin : BaseUnityPlugin
 {
     internal static ManualLogSource Log { get; private set; } = null!;
-    private static TextMeshProUGUI progressCircleText;
     private static GUIManager guiManager;
+    private static TextMeshProUGUI itemUseTimeTextMesh;
     private static ConfigEntry<float> configFontSize;
     private static ConfigEntry<float> configOutlineWidth;
 
@@ -21,7 +21,7 @@ public partial class Plugin : BaseUnityPlugin
     {
         Log = Logger;
         configFontSize = ((BaseUnityPlugin)this).Config.Bind<float>("ItemUseTime", "Font Size", 20f, "Customize the Font Size for item progress circle text.");
-        configOutlineWidth = ((BaseUnityPlugin)this).Config.Bind<float>("ItemUseTime", "Outline Width", 0.1f, "Customize the Outline Width for item progress circle text.");
+        configOutlineWidth = ((BaseUnityPlugin)this).Config.Bind<float>("ItemUseTime", "Outline Width", 0.08f, "Customize the Outline Width for item progress circle text.");
         Harmony.CreateAndPatchAll(typeof(ItemUseTimeProgressUpdatePatch));
         Log.LogInfo($"Plugin {Name} is loaded!");
     }
@@ -40,7 +40,7 @@ public partial class Plugin : BaseUnityPlugin
                 }
                 else
                 {
-                    updateProgressCircleText(__instance);
+                    updateitemUseTimeTextMesh(__instance);
                 }
             }
             catch (Exception e)
@@ -50,75 +50,79 @@ public partial class Plugin : BaseUnityPlugin
         }
     }
 
-    private static void updateProgressCircleText(UI_UseItemProgress useItemProgress)
+    private static void updateitemUseTimeTextMesh(UI_UseItemProgress useItemProgress)
     {
-        //Character character = guiManager.character;
         Character character = Character.observedCharacter;
 
         if (character != null)
         {
             if (character.photonView.IsMine)
-            { 
-                if (character.refs.items.climbingSpikeCastProgress > 0f)
+            {
+                Item currItem = character.data.currentItem;
+
+                if (currItem.gameObject.TryGetComponent<RopeTier>(out RopeTier ropeTier))
                 {
-                    // TODO: find better access point for climbing spike conditional
-                    // Climbing Spike Use Time currently hard set at 2f (can't find a good var to access due to temporary hand slot edge case not updating .currentClimbingSpikeItemSlot)
-                    // might just search the scene with find for the first climbing spike within game scene...? but highly likely to break if it checks someone else's while they're using it
-                    progressCircleText.text = ((1f - useItemProgress.fill.fillAmount) * 2f).ToString("F1");
-                    progressCircleText.gameObject.SetActive(true);
+                    itemUseTimeTextMesh.text = ((1f - useItemProgress.fill.fillAmount) * ropeTier.castTime).ToString("F1");
+                    itemUseTimeTextMesh.gameObject.SetActive(true);
                 }
-                else if ((character.data.currentItem != null) && (character.data.currentItem.shouldShowCastProgress))
+                else if ((character.refs.items.climbingSpikeCastProgress > 0f) && (character.refs.items.currentClimbingSpikeComponent.gameObject.TryGetComponent<Item>(out Item climbingSpike)))
                 {
-                    if (character.data.currentItem.isUsingSecondary)
+                    itemUseTimeTextMesh.text = ((1f - useItemProgress.fill.fillAmount) * climbingSpike.totalSecondaryUsingTime).ToString("F1");
+                    itemUseTimeTextMesh.gameObject.SetActive(true);
+                }
+                else if ((currItem != null) && (currItem.shouldShowCastProgress))
+                {
+                    if (currItem.isUsingSecondary)
                     {
-                        progressCircleText.text = ((1f - useItemProgress.fill.fillAmount) * character.data.currentItem.totalSecondaryUsingTime).ToString("F1");
+                        itemUseTimeTextMesh.text = ((1f - useItemProgress.fill.fillAmount) * currItem.totalSecondaryUsingTime).ToString("F1");
                     }
                     else
                     {
-                        progressCircleText.text = ((1f - useItemProgress.fill.fillAmount) * character.data.currentItem.usingTimePrimary).ToString("F1");
+                        itemUseTimeTextMesh.text = ((1f - useItemProgress.fill.fillAmount) * currItem.usingTimePrimary).ToString("F1");
                     }
-                    progressCircleText.gameObject.SetActive(true);
+                    itemUseTimeTextMesh.gameObject.SetActive(true);
                 }
                 else if ((useItemProgress.constantUseInteractableExists) && (Interaction.instance.constantInteractableProgress > 0f))
                 {
-                    progressCircleText.text = ((1f - useItemProgress.fill.fillAmount) * Interaction.instance.currentConstantInteractableTime).ToString("F1");
-                    progressCircleText.gameObject.SetActive(true);
+                    itemUseTimeTextMesh.text = ((1f - useItemProgress.fill.fillAmount) * Interaction.instance.currentConstantInteractableTime).ToString("F1");
+                    itemUseTimeTextMesh.gameObject.SetActive(true);
                 }
                 else
                 {
-                    progressCircleText.gameObject.SetActive(false);
+                    itemUseTimeTextMesh.gameObject.SetActive(false);
                 }
             }
             else
             {
-                progressCircleText.gameObject.SetActive(false);
+                itemUseTimeTextMesh.gameObject.SetActive(false);
             }
         }
         else
         {
-            progressCircleText.gameObject.SetActive(false);
+            itemUseTimeTextMesh.gameObject.SetActive(false);
         }
     }
 
     private static void InitItemUseTime()
     {
         GameObject guiManagerGameObj = GameObject.Find("GAME/GUIManager");
-        guiManager = guiManagerGameObj.GetComponent<GUIManager>();
-        TMPro.TMP_FontAsset font = guiManager.heroDayText.font;
         GameObject useItemGameObj = guiManagerGameObj.transform.Find("Canvas_HUD/UseItem").gameObject;
+        TMPro.TMP_FontAsset font = guiManager.heroDayText.font;
+        guiManager = guiManagerGameObj.GetComponent<GUIManager>();
+
         GameObject itemUseTime = new GameObject("ItemUseTime");
         itemUseTime.transform.SetParent(useItemGameObj.transform);
-        TextMeshProUGUI itemUseTimeText = itemUseTime.AddComponent<TextMeshProUGUI>();
+        itemUseTimeTextMesh = itemUseTime.AddComponent<TextMeshProUGUI>();
         RectTransform itemUseTimeRect = itemUseTime.GetComponent<RectTransform>();
-        itemUseTimeText.font = font;
-        itemUseTimeText.fontSize = configFontSize.Value;
+
+        itemUseTimeTextMesh.font = font;
+        itemUseTimeTextMesh.fontSize = configFontSize.Value;
         itemUseTimeRect.offsetMin = new Vector2(0f, 0f);
         itemUseTimeRect.offsetMax = new Vector2(0f, 0f);
-        itemUseTimeText.alignment = TextAlignmentOptions.Center;
-        itemUseTimeText.verticalAlignment = VerticalAlignmentOptions.Capline;
-        itemUseTimeText.textWrappingMode = TextWrappingModes.NoWrap;
-        itemUseTimeText.text = "";
-        progressCircleText = itemUseTimeText;
-        itemUseTimeText.outlineWidth = configOutlineWidth.Value;
+        itemUseTimeTextMesh.alignment = TextAlignmentOptions.Center;
+        itemUseTimeTextMesh.verticalAlignment = VerticalAlignmentOptions.Capline;
+        itemUseTimeTextMesh.textWrappingMode = TextWrappingModes.NoWrap;
+        itemUseTimeTextMesh.text = "";
+        itemUseTimeTextMesh.outlineWidth = configOutlineWidth.Value;
     }
 }
